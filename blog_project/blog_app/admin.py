53fb2e2bda4f08da42998027b675c346e8eb71fc
nashid1487 <84094880+nashid1487@users.noreply.github.com>
2021-07-17 11:00:16 +0530

    Update admin.py

--- blog_project/blog_app/admin.py
+++ blog_project/blog_app/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 from .models import Post
 
 # Register your models here.
+
+# To format the admin panel to display necessary data
 class PostAdmin(admin.ModelAdmin):
-    list_display = ('title', 'slug', 'status', 'created_on')
+    list_display = ('title', 'slug', 'status', 'created_on') 
     list_filter = ("status",)
     search_fields = ['title', 'content']
     prepopulated_fields = {'slug': ('title',)}
